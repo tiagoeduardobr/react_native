663d01ec29c7eadad6a404c1109b9eb00a9a5f13
Tiago Eduardo Zimmermann <tiagoeduardobr@gmail.com>
2026-07-08 22:09:46 -0300

    chore: organize study files and add gitignore

---
+++ Semana-02/sintaxe-basica.js
@@ -0,0 +1,6 @@
+let nomeFuncionario = "Carlos Mendes";
+let salarioBase = 3200;
+salarioBase = salarioBase * 1.15;
+
+console.log("Funcionário: " + nomeFuncionario);
+console.log("Salário reajustado: R$ " + salarioBase.toFixed(2));
